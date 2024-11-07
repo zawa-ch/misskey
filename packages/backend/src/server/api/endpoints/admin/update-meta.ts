09ccf389ec2e48e9d04976c91f2c58b1ebd50df2
zawa-ch <satellite.2e1834097@gmail.com>
2024-11-07 16:56:31 +0000

    e2eテストの更新

--- packages/backend/src/server/api/endpoints/admin/update-meta.ts
+++ packages/backend/src/server/api/endpoints/admin/update-meta.ts
@@ -46,7 +46,7 @@ export const paramDef = {
 				type: 'string',
 			},
 		},
-		prohibitedNotePattern: { type: 'object', nullable: true },
+		prohibitedNotePattern: { type: 'object', nullable: true, ref: 'ProhibitedNoteFormulaValue' },
 		themeColor: { type: 'string', nullable: true, pattern: '^#[0-9a-fA-F]{6}$' },
 		mascotImageUrl: { type: 'string', nullable: true },
 		bannerUrl: { type: 'string', nullable: true },
